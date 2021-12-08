b5d7ef1d6a6c1c9830af444bf7c946df160bf885
Bagoes Siswo Febrianto <bass.sys@gmail.com>
2021-12-08 09:34:17 +0700

    [UPDATE] secure asset

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -12,13 +12,13 @@
     <title>ASIPS</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 </head>
 
@@ -35,7 +35,7 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background-image: url({{ asset('img/admin/login.jpg') }}) !important"></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background-image: url({{ secure_asset('img/admin/login.jpg') }}) !important"></div>
                             <div class="col-lg-6">
                                 <div style="padding: 7rem 5rem;">
                                     <div class="text-center">
@@ -68,14 +68,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ secure_asset('js/sb-admin-2.min.js') }}"></script>
 
 </body>
 
